dd59b4b428b7332ade138f0bbde7ec6bece4a01f
Serenity <70578351+fmcpe@users.noreply.github.com>
2026-06-23 02:41:50 +0700

    Convert MouseInfoAccessor.java to Kotlin

---
+++ src/main/kotlin/com/github/joonasvali/naturalmouse/api/MouseInfoAccessor.kt
@@ -0,0 +1,16 @@
+package com.github.joonasvali.naturalmouse.api
+
+import java.awt.Point
+
+/**
+ * Abstraction for getting mouse position.
+ */
+interface MouseInfoAccessor {
+  /**
+   * Get the current mouse position.
+   * NB, for optimization reasons this method might return the same Point instance, but is not quaranteed to.
+   * It is recommended not to save this Point anywhere as it may or may not change its coordinates.
+   * @return the current mouse position
+   */
+  fun getMousePosition(): Point
+}
